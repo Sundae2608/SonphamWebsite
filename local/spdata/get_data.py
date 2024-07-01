89f10ea85ed61b2868a194e9a6d942c1b500ac86
cellensinc <135358973+cellensinc@users.noreply.github.com>
2024-07-01 02:32:33 -0400

    first commit

---
+++ local/spdata/get_data.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Fetch the web page
+url = "https://shillerdata.com/"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Find all links with the specified data-aid attribute
+links = soup.find_all('a', {'data-aid': 'DOWNLOAD_DOCUMENT_LINK_RENDERED'})
+
+# Loop through the links and find the one with 'ie_data.xls' in the href
+file_url = None
+for link in links:
+    href = link['href']
+    if 'ie_data.xls' in href:
+        file_url = href
+        break
+
+# Complete the URL if it's relative
+if file_url and file_url.startswith("//"):
+    file_url = "https:" + file_url
+
+if file_url:
+    print("File URL:", file_url)
+
+    # Download the file
+    file_response = requests.get(file_url)
+    with open("spdata/ie_data.xls", "wb") as file:
+        file.write(file_response.content)
+
+    print("File downloaded successfully!")
+else:
+    print("File not found.")
---
+++ local/spdata/process_data.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+# Load the Excel file
+file_path = "spdata/ie_data.xls"
+xls = pd.ExcelFile(file_path)
+
+# Load the sheet named 'Data' into a DataFrame, skipping the first 7 rows
+df = xls.parse('Data', skiprows=7, dtype={'Date': str})
+
+
+# Drop the last row
+df = df[:-1]
+
+# Select only the necessary columns
+df = df[['Date', 'P', 'D', 'CPI']]
+
+
+# Function to process the date column
+def process_date(date_str):
+    # Check if the date ends with ".1"
+    if date_str.endswith('.1'):
+        # Replace ".1" with ".10"
+        return date_str.replace('.1', '.10')
+    return date_str
+
+
+# Apply the function to the 'Date' column
+df['Date'] = df['Date'].apply(process_date)
+
+# Convert to date time
+df["Date"] = pd.to_datetime(df["Date"], format="%Y.%m")
+df["Date"] = df["Date"].dt.strftime("%Y-%m")
+
+# Convert the DataFrame to JSON format
+json_data = df.to_json(orient='records')
+
+# Save the JSON data to a file
+json_file_path = "../spdata.json"
+with open(json_file_path, "w") as json_file:
+    json_file.write(json_data)
+
+print("Data saved to spdata.json")
